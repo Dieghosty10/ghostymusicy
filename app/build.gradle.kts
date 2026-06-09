f69adf89bef67dc79c229d7d10395bc5f76779fe
Diego Torrez <dieghosty10@example.com>
2026-06-09 00:08:14 -0400

    chore: bump version to 3.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.dieghosty10.ghostymusicy"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3007
-        versionName = "3.0.7"
+        versionCode = 3008
+        versionName = "3.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
